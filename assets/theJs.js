e93865793eefb50ba242fb045dcb5558e40745b3
Anton Economos <anton.economos@gmail.com>
2016-11-12 19:24:55 -0500

    Revamping portfolio site. Initial commit.

---
+++ assets/theJs.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+//smooth scrolls to link without changing the url
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000);
+        return false;
+        }
+      }
+    });
+  });
+
+$("#helloContent1").fadeIn(2000).promise().done(function() {
+    $('#helloContent2').fadeIn(2000);
+});
+
+})
