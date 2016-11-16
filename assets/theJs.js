9edc8171e593d84b83ceb2cd4ff1699c8d0d95b3
Anton Economos <anton.economos@gmail.com>
2016-11-15 23:12:40 -0500

    new scrolling ability for projects section. removed slickjs, used pure js

--- assets/theJs.js
+++ assets/theJs.js
@@ -20,18 +20,55 @@ $("#helloContent1").fadeIn(2000).promise().done(function() {
     $('#helloContent2').fadeIn(2000);
 });
 
-$(".projectNav").slick({
-  dots: true,
-  infinite: true,
-  asNavFor: "#projectsContent",
-})
+var slideIndex = 1;
+showDivs(slideIndex);
 
-$("#projectsContent").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
 
-})
+function showDivs(n) {
+    var x = document.getElementsByClassName("projectTitle");
+    if (n > x.length){
+      slideIndex = 1
+    }
+    else if (n < 1){
+      slideIndex = x.length
+    };
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block";
+    var y = document.getElementsByClassName("projectDescribe");
+    if (n > y.length){
+      slideIndex = 1
+    }
+    else if (n < 1){
+      slideIndex = y.length
+    };
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none"; 
+    }
+    y[slideIndex-1].style.display = "block";
+    var z = document.getElementsByClassName("project");
+    if (n > z.length){
+      slideIndex = 1
+    }
+    else if (n < 1){
+      slideIndex = z.length
+    };
+    for (i = 0; i < z.length; i++) {
+        z[i].style.display = "none"; 
+    }
+    z[slideIndex-1].style.display = "block"; 
+}
+
+$("#prevArrow").click(function(){
+    plusDivs(-1)
+});
+
+$("#nextArrow").click(function(){
+  plusDivs(1)
+});
 
 })
